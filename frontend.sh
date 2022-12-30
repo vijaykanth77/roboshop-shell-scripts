3c9cb9cf96c0e07b37793ead06b501fd77b9c1b4
vijayaws9775 <reddyvijaykanth24@gmail.com>
2022-12-30 08:00:25 +0530

    Added Frontend script

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,19 @@
+echo installing nginx
 yum install nginx -y
 systemctl enable nginx
-systemctl start nginx
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+echo downloading ngimx
+crl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
 cd /usr/share/nginx/html
+
+echo removing web content
 rm -rf *
+
+
 unzip /tmp/frontend.zip
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+
+eecho starting nginx service
 systemctl restart nginx
