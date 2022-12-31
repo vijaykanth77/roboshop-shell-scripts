efb74ef397c1106b4326ee7c633184a3e837f40c
vijayaws9775 <reddyvijaykanth24@gmail.com>
2022-12-31 15:33:18 +0530

    MongoDB script

--- mongodb.sh
+++ mongodb.sh
@@ -8,7 +8,7 @@ yum install -y mongodb-org &>>$LOG_FILE
 echo status = $?
 
 echo 'update Mongo Db Listen address'
-sed -i -e 's/127/0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo status = $?
 
 echo 'Starting MongoDB Service'
