ef7d0860779e295a715d877098c5bc78721dd56e
vijayaws9775 <reddyvijaykanth24@gmail.com>
2022-12-31 17:03:03 +0530

    MongoDB script

--- mongodb.sh
+++ mongodb.sh
@@ -7,14 +7,41 @@ echo 'Installing MongoDB server'
 yum install -y mongodb-org &>>$LOG_FILE
 echo status = $?
 
-echo 'update MongoDB Listen address'
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-echo status = $?
-
 echo 'Starting MongoDB Service'
 systemctl enable mongod &>>$LOG_FILE
 systemctl start mongod &>>$LOG_FILE
 echo status = $?
+
+
+echo 'update MongoDB Listen address'
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+echo status = $?
+
+
+systemctl restart mongod &>>$LOG_FILE
+echo status = $?
+
+echo "Download Mongodb Schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
+echo status = $?
+
+cd /tmp
+
+echo "Extract Schema file"
+unzip mongodb.zip &>>$LOG_FILE
+echo status = $?
+
+cd mongodb-main
+
+echo "Load catalogue service schema"
+mongo < catalogue.js &>>$LOG_FILE
+echo status = $?
+
+echo "Load users service schema"
+mongo < users.js &>>$LOG_FILE
+echo status = $?
+
+
 
 
 
