fbd822bc812f8213ca5c6eaefd70047397d1bbad
vijayaws9775 <reddyvijaykanth24@gmail.com>
2022-12-31 14:52:52 +0530

    Added Frontend script

--- frontend.sh
+++ frontend.sh
@@ -1,27 +1,28 @@
+LOG_FILE=/tmp/frontend
 echo installing nginx
-yum install nginx -y &>>/tmp/frontend
+yum install nginx -y &>>$LOG_FILE
 echo status = $?
 
-systemctl enable nginx &>>/tmp/frontend
-systemctl start nginx &>>/tmp/frontend
+systemctl enable nginx &>>$LOG_FILE
+systemctl start nginx &>>$LOG_FILE
 
 echo downloading ngimx
-crl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/frontend
+crl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
 echo status = $?
 
-cd /usr/share/nginx/html &>>/tmp/frontend
+cd /usr/share/nginx/html &>>$LOG_FILE
 
 echo removing web content
-rm -rf * &>>/tmp/frontend
+rm -rf * &>>$LOG_FILE
 echo status = $?
 
 echo extracting the web content
-unzip /tmp/frontend.zip &>>/tmp/frontend
+unzip /tmp/frontend.zip &>>$LOG_FILE
 echo status = $?
 
-mv frontend-main/static/* . &>>/tmp/frontend
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/frontend
+mv frontend-main/static/* . &>>$LOG_FILE
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 
 eecho starting nginx service
-systemctl restart nginx &>>/tmp/frontend
+systemctl restart nginx &>>$LOG_FILE
 echo status = $?
