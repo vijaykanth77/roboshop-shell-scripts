18589a9773aab2eb11ccabfa42146681c87aa24b
vijayaws9775 <reddyvijaykanth24@gmail.com>
2022-12-31 15:31:53 +0530

    MongoDB script

--- mongodb.sh
+++ mongodb.sh
@@ -1,13 +1,17 @@
 LOG_FILE=/tmp/mongodb
-echo 'setting Mongo DB db repo'
+echo 'setting MongoDB repo'
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
 echo status = $?
 
-echo 'Installing Mongo DB server'
+echo 'Installing MongoDB server'
 yum install -y mongodb-org &>>$LOG_FILE
 echo status = $?
 
-echo 'Starting Mongo DB Service'
+echo 'update Mongo Db Listen address'
+sed -i -e 's/127/0.0.1/0.0.0.0/' /etc/mongod.conf
+echo status = $?
+
+echo 'Starting MongoDB Service'
 systemctl enable mongod &>>$LOG_FILE
 systemctl start mongod &>>$LOG_FILE
 echo status = $?
