70eacd661d623e81162a438734cdd17a4a342299
vijayaws9775 <reddyvijaykanth24@gmail.com>
2023-01-02 17:48:58 +0530

    Catalogue script

--- frontend.sh
+++ frontend.sh
@@ -7,7 +7,7 @@ systemctl enable nginx &>>$LOG_FILE
 systemctl start nginx &>>$LOG_FILE
 
 echo downloading ngimx
-crl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
 echo status = $?
 
 cd /usr/share/nginx/html &>>$LOG_FILE
